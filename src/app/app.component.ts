9ab94ae49619f647ff9bcb6afa8b318621538c9c
CHBaker <charles.baker@besl.org>
2018-04-11 11:27:38 -0600

    FEATURE/UI: add pill animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { routerTransition } from './router.animations';
-import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    animations: [routerTransition]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations: [routerTransition]
 })
 export class AppComponent {
+    @ViewChild('home') home: ElementRef;
+    @ViewChild('work') work: ElementRef;
+
+    constructor(private renderer: Renderer2) {}
+
     getState(outlet) {
         return outlet.activatedRouteData.state;
+    }
+
+    // animate pills
+    flowToWork() {
+        this.renderer.addClass(this.work.nativeElement, 'flow-work');
+        this.renderer.addClass(this.home.nativeElement, 'flow-out-home');
+    }
+
+    flowToHome() {
+        this.renderer.addClass(this.home.nativeElement, 'flow-home');
+        this.renderer.addClass(this.work.nativeElement, 'flow-out-work');
     }
 }
