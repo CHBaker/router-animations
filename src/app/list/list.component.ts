ea80c79ad2da62d9d1ba4cc07c48eb8d277f60c6
CHBaker <charles.baker@besl.org>
2018-04-24 10:25:42 -0600

    FEATURE: save notes in local storage
    - Delete Note functionality
    - Save notes in local
    - Check local on init for notes

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -6,40 +6,58 @@ import { FormControl, Validators } from '@angular/forms';
     templateUrl: './list.component.html',
     styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
     note: FormControl;
     formReady = false;
     notes = [
+        'vacuum the cat',
         'relax on the sofa',
         'make dinner',
         'chill or be chilled'
     ];
 
     ngOnInit(): void {
+        this.checkLocalStorage();
         this.initListForm();
     }
 
+    checkLocalStorage() {
+        if (this.LocalStorage !== null) {
+            this.notes = this.LocalStorage;
+        }
+    }
+
     initListForm(): void {
-        this.note = new FormControl(
-            '', Validators.required
-        );
+        this.note = new FormControl('', Validators.required);
         this.formReady = true;
+    }
+
+    get LocalStorage() {
+        return JSON.parse(localStorage.getItem('notes'));
+    }
+
+    set LocalStorage(notes: string[]) {
+        localStorage.setItem('notes', JSON.stringify(notes));
     }
 
     get newNote() {
         return this.note.value;
     }
 
-    set newNote(note: string) {
-        this.note.setValue(note);
-    }
-
     addNote(): void {
-        this.notes.unshift(this.newNote);
-        this.note.reset();
+        if (this.note.valid) {
+            this.notes.unshift(this.newNote);
+            this.note.reset();
+            this.LocalStorage = this.notes;
+        }
     }
 
     deleteNote(note: string): void {
-        this.notes.filter(currNote => currNote !== note);
+        this.notes = this.notes.filter(currNote => currNote !== note);
+        this.LocalStorage = this.notes;
+    }
+
+    ngOnDestroy() {
+        this.LocalStorage = this.notes;
     }
 }
