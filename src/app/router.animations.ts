c94feef8d02f13ca698630c532d4d3ed7885d72a
CHBaker <charles.baker@besl.org>
2018-04-24 09:26:40 -0600

    STYLE: animate clouds

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -17,11 +17,11 @@ export const routerTransition = trigger('routerTransition', [
     /* 3 */ group([ // block executes in parellel
         query(':enter', [
             style({ transform: 'translateX(100%)' }),
-            animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+            animate('1s ease-in-out', style({ transform: 'translateX(0%)' }))
         ], { optional: true }),
         query(':leave', [
             style({ transform: 'translateX(0%)' }),
-            animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
+            animate('1s ease-in-out', style({ transform: 'translateX(-100%)' }))
         ], { optional: true })
     ])
   ])
