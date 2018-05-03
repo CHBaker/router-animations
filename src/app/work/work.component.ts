7e39aa64b9ebb3a3bd616378a6e6b075e26ceca1
CHBaker <charles.baker@besl.org>
2018-05-03 09:19:25 -0600

    FEATURE: light switch
    - Turn lamp light off and on

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-work',
-  templateUrl: './work.component.html',
-  styleUrls: ['./work.component.css']
+    selector: 'app-work',
+    templateUrl: './work.component.html',
+    styleUrls: ['./work.component.css']
 })
 export class WorkComponent implements OnInit {
+    @ViewChild('lampLight') lampLight: ElementRef;
 
-  constructor() { }
+    buttonOn = true;
 
-  ngOnInit() {
-  }
+    constructor(private renderer: Renderer2) {}
 
+    ngOnInit() {
+        this.renderer.addClass(this.lampLight.nativeElement, 'animate-light');
+    }
+
+    turnLight() {
+        this.buttonOn = !this.buttonOn;
+        this.renderer.removeClass(this.lampLight.nativeElement, 'animate-light');
+        if (this.buttonOn) {
+            console.log('light on')
+            this.renderer.removeClass(this.lampLight.nativeElement, 'lamp-off');
+            this.renderer.addClass(this.lampLight.nativeElement, 'lamp-on');
+        } else {
+            console.log('light off')
+            this.renderer.removeClass(this.lampLight.nativeElement, 'lamp-on');
+            this.renderer.addClass(this.lampLight.nativeElement, 'lamp-off');
+        }
+    }
 }
